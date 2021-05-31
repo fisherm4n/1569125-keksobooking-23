c1bf8c92074d432b04213f7c52c9f7b95dbcf9e1
fisherm4n <4143322@inbox.ru>
2021-06-01 01:56:57 +0300

    random functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const randomValue = (min,max) =>{
+  if (min < 0 || max < 0 ){
+    return console.log('Вы ввели отрицательное число');
+  }
+  if (max<=min){
+    return console.log('Вы ввели некорректный диапозон');
+
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+randomValue (0 , 100);
+
+const randomFloat = (min, max, afterDot) =>{
+  if (min < 0 || max < 0) {
+    return console.log('Вы ввели отрицательное число');
+  }
+  if (max <= min) {
+    return console.log('Вы ввели некорректный диапозон');
+  }
+  let randomResult = Math.random() * (max - min + 1) + min;
+  return randomResult.toFixed(afterDot);
+}
+randomFloat(2, 98, 1);
