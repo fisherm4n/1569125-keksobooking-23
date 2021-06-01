d839b59d1d058d4789a1cbd9ad3f7af368ccfcb1
fisherm4n <4143322@inbox.ru>
2021-06-01 21:59:37 +0300

    syntax fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-function randomValue (min, max){
-  if (min < 0 || max < 0 ){
+function randomValue(min, max) {
+  if (min < 0 || max < 0) {
     return 'Вы ввели отрицательное число';
   }
-  if (max <= min){
+  if (max <= min) {
     return 'Вы ввели некорректный диапозон';
 
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-randomValue (0 , 100);
+randomValue(0, 100);
 
-function randomFloat (min, max, afterDot){
+function randomFloat(min, max, afterDot) {
   if (min < 0 || max < 0) {
     return 'Вы ввели отрицательное число';
   }
