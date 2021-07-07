4d05d8f546a3af96dc984a28a3d3e2fff8d3a260
fisherm4n <4143322@inbox.ru>
2021-07-08 02:12:28 +0300

    Добавил валидацию для заголовка, описания, цены за ночь и для гостей с комнатами

--- js/form-activation.js
+++ js/form-activation.js
@@ -11,4 +11,41 @@ const activateForm = function (el) {
     item.disabled = false;
   });
 };
-export { disableForm, activateForm };
+const form = document.querySelector('.ad-form');
+const selectType = form.querySelector('#type');
+const inputPrice = form.querySelector('#price');
+const selectRooms = form.querySelector('#room_number');
+const selectGuests = form.querySelector('#capacity');
+const MINPRICE_FOR_TYPE = {
+  bungalow:0,
+  flat:1000,
+  hotel:3000,
+  house:5000,
+  palace:10000,
+};
+
+const onRoomToGuestCheck = function(){
+  const room = selectRooms.value;
+  const guest = selectGuests.value;
+  if(room === '1' && guest !=='1' ){
+    selectGuests.setCustomValidity('Однокомнатная только для одного гостя.');
+  } else if (room === '100' && guest !== '0'){
+    selectGuests.setCustomValidity('Не для гостей');
+  } else if (room === '2' && guest !== '2' && guest !== '1') {
+    selectGuests.setCustomValidity('До двух гостей включительно');
+  } else if (room === '3' && guest !== '3' && guest !== '2' && guest !== '1') {
+    selectGuests.setCustomValidity('До трёх гостей включительно');
+  } else{
+    selectGuests.setCustomValidity('');
+  }
+  selectGuests.reportValidity();
+}
+selectGuests.addEventListener('change',onRoomToGuestCheck);
+selectRooms.addEventListener('change',onRoomToGuestCheck);
+const onPriceChange = function(){
+  inputPrice.placeholder = MINPRICE_FOR_TYPE[selectType.value];
+  inputPrice.min = MINPRICE_FOR_TYPE[selectType.value];
+};
+selectType.addEventListener('change', onPriceChange );
+export { disableForm, activateForm};
+
